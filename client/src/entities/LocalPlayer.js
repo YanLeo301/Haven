405d587dac534fcaae833edb86add89de8ad3e1b
Yan Leo <yan.leo@uni-rostock.de>
2026-08-19 18:23:58 +0200

    Refactor LocalPlayer to be a swarm of circles, Change from arcade to matter physics

--- client/src/entities/LocalPlayer.js
+++ client/src/entities/LocalPlayer.js
@@ -1,61 +1,116 @@
 import Phaser from 'phaser'
 
-const playerWidth = 20;
-const playerHeight = 20;
+//TODO: find the best feeling values
+const playerRadius = 20;
 const playerColor = 0xffffff;
-const playerSpeed = 300;
+const playerSpeed = 5;
 
-export default class LocalPlayer extends Phaser.GameObjects.Rectangle // -> Phaser.GameObjects.Sprite
+const droneRadius = 5;
+const droneSeparationDistance = 5;
+const attractionStrength = 0.000002;
+
+const PLAYER_CATEGORY = 0x0001;
+const DRONE_CATEGORY = 0x0002;
+
+export default class LocalPlayer extends Phaser.GameObjects.Arc
 {
     constructor(scene, x, y, network)
     {
-        super(scene, x, y, playerWidth, playerHeight, playerColor);
+        super(scene, x, y, playerRadius, 0, 360, false, 0x000000, 1);
 
         scene.add.existing(this);
+        scene.matter.add.gameObject(this);
 
-        scene.physics.world.enable(this);
-        this.body.setCollideWorldBounds(true);
-
+        this.body.circleRadius = playerRadius;
+        this.body.collisionFilter.category = PLAYER_CATEGORY;
+        this.body.collisionFilter.mask = PLAYER_CATEGORY;
+        
         this.network = network;
 
         this.speed = playerSpeed;
 
-        //TODO: figure out why this is not working
-        // this.network.send(JSON.stringify({
-        //     type: "PLAYER_POS",
-        //     x: this.body.position.x,
-        //     y: this.body.position.y
-        // }));
-    }
+        this.keys = scene.input.keyboard.addKeys('W,A,S,D');
 
+        this.drones = [];
+
+        for (let i = 0; i < 20; i++) this.addDrone();
+    }
+    
     setId(id)
     {
         this.id = id;
         console.log("Local player id set to: ", id);
     }
 
+    addDrone() 
+    {
+        const spawnAngle = Phaser.Math.FloatBetween(0, Math.PI * 2);
+        const spawnDist = Phaser.Math.FloatBetween(playerRadius, playerRadius + 30);
+
+        const drone = this.scene.add.circle(
+            this.x + Math.cos(spawnAngle) * spawnDist,
+            this.y + Math.sin(spawnAngle) * spawnDist,
+            droneRadius,
+            playerColor
+        )
+
+        this.scene.matter.add.gameObject(
+            drone,
+            {shape: 
+            {
+                type: 'circle',
+                radius: droneRadius + 3
+            }}
+        );
+        drone.body.circleRadius = droneRadius;
+
+        drone.setBounce(1);
+        drone.setFrictionAir(Phaser.Math.FloatBetween(0.03, 0.06));
+        drone.setMass(Phaser.Math.FloatBetween(0.8, 1.2));
+
+        drone.body.collisionFilter.category = DRONE_CATEGORY;
+        drone.body.collisionFilter.mask = DRONE_CATEGORY;
+
+        this.drones.push(drone);
+    }
+
+    // TODO: make it feel more dynamic
+    updateSwarm()
+    {
+        for (const drone of this.drones)
+        {
+            const dx = this.x - drone.x;
+            const dy = this.y - drone.y;
+
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance > playerRadius)
+            {
+                const dirX = dx / distance;
+                const dirY = dy / distance;
+
+                let forceMagnitude = (distance - playerRadius) * attractionStrength;
+
+                const maxForce = 0.005;
+                forceMagnitude = Math.min(forceMagnitude, maxForce);
+
+                drone.applyForce({x: dirX * forceMagnitude, y: dirY * forceMagnitude});
+            }
+        }
+    }
+
     update()
     {
         var vx = 0, vy = 0;
 
-        if (this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W).isDown) 
-        {
-            vy = -this.speed;
-        }
-        if (this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A).isDown) 
-        {
-            vx = -this.speed;
-        }
-        if (this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S).isDown) 
-        {
-            vy = this.speed;
-        }
-        if (this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D).isDown) 
-        {
-            vx = this.speed;
-        }
+        if (this.keys.W.isDown) vy = -this.speed;
+        if (this.keys.A.isDown) vx = -this.speed;
+        if (this.keys.S.isDown) vy = this.speed;
+        if (this.keys.D.isDown) vx = this.speed;
 
-        this.body.setVelocity(vx, vy);
+        this.setVelocity(vx, vy);
+
+        this.updateSwarm();
 
         //TODO: only send when position changes, but send spawn position
         this.network.send(JSON.stringify({
--- client/src/main.js
+++ client/src/main.js
@@ -5,14 +5,37 @@ import WebSocketClient from './network/WebSocketClient';
 var webSocketClient = new WebSocketClient();
 webSocketClient.connect('http://localhost:8080/');
 
+const width = 1000;
+const height = 600;
+
 var config =
 {
     type: Phaser.AUTO,
-    width: 1000,
-    height: 600,
+    width: width,
+    height: height,
     physics:
     {
-        default: 'arcade'
+        default: 'matter',
+        matter:
+        {
+            gravity:
+            {
+                x: 0,
+                y: 0,
+            },
+            setBounds:
+            {
+                x: 0,
+                y: 0,
+                width: width,
+                height: height,
+                thickness: 64,
+                left: true,
+                right: true,
+                top: true,
+                bottom: true,
+            }
+        }
     },
     scene: new MainScene(webSocketClient, webSocketClient.messageHandler)
 };
